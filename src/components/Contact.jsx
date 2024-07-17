f79962e506497cf6c8cf1b392e5a7770f6565ed5
Mariya369 <mariyadotkova555@gmail.com>
2024-07-17 10:43:10 +0200

    form fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -72,52 +72,52 @@ const Contact = () => {
       >
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
-        <form ref={formRef} onSubmit={handleSubmit} className='mt-12 flex flex-col gap-8'>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Name</span>
-            <input
-              type='text'
-              id="name"
-              name='name'
-              value={form.name}
-              onChange={handleChange}
-              placeholder="What's your name?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your email</span>
-            <input
-              type='email'
-              id="email"
-              name='email'
-              value={form.email}
-              onChange={handleChange}
-              placeholder="What's your email address?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Message</span>
-            <textarea
-              rows={7}
-              id="message"
-              name='message'
-              value={form.message}
-              onChange={handleChange}
-              placeholder='Please, feel free to write your questions or comment!'
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
+     <form ref={formRef} onSubmit={handleSubmit} className='mt-12 flex flex-col gap-8'>
+        <label className='flex flex-col'>
+        <span className='text-white font-medium mb-4'>Your Name</span>
+         <input
+      type='text'
+      id="name"
+      name='name'
+      value={form.name}
+      onChange={handleChange}
+      placeholder="What's your name?"
+      className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+    />
+        </label>
+        <label className='flex flex-col'>
+    <span className='text-white font-medium mb-4'>Your email</span>
+    <input
+      type='email'
+      id="email"
+      name='email'
+      value={form.email}
+      onChange={handleChange}
+      placeholder="What's your email address?"
+      className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+    />
+  </label>
+  <label className='flex flex-col'>
+    <span className='text-white font-medium mb-4'>Your Message</span>
+    <textarea
+      rows={7}
+      id="message"
+      name='message'
+      value={form.message}
+      onChange={handleChange}
+      placeholder='Please, feel free to write your questions or comment!'
+      className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+    />
+  </label>
+  <button
+    type='submit'
+    className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+    disabled={loading}
+  >
+    {loading ? "Sending..." : "Send"}
+  </button>
+</form>
 
-          <button
-            type='submit'
-            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
-            disabled={loading}
-          >
-            {loading ? "Sending..." : "Send"}
-          </button>
-        </form>
       </motion.div>
       <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
