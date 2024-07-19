cd385a774c2c17bb49b62206ddcf67f3b0813b12
Mariya369 <mariyadotkova555@gmail.com>
2024-07-19 22:13:39 +0200

    Debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,30 @@
-import { BrowserRouter } from "react-router-dom"
-import { About, Contact, Hero, Navbar, Tech, Projects, PricingPlans, StarsCanvas } from './components';
+import { BrowserRouter } from "react-router-dom";
+import { About, Contact, Hero, Navbar, Tech, Projects, PricingPlans } from './components';
+import { StarsCanvas } from "../canvas/StarsCanvas";
 import SecondaryNav from './components/SecondaryNav';
 
 const App = () => {
-
   return (
     <BrowserRouter>
-    <div className="relative z-0 bg-primary">
-      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-      <Navbar />
-        <Hero />
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Tech />
+        <Projects />
+        <PricingPlans />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+        <div className="relative z-0 flex justify-center items-center h-screen">
+          <SecondaryNav />
+        </div>
       </div>
-      <About />
-      <Tech />
-      <Projects />
-      <PricingPlans />
-      <div className="relative z-0">
-        <Contact />
-        <StarsCanvas />
-      </div>
-      <div className="relative z-0 flex justify-center items-center h-screen">
-      <SecondaryNav />
-      <StarsCanvas />
-      </div>
-    </div>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,32 +1,32 @@
 import logo from "/assets/logo500.png";
-import frontend from "./frontend.png";
-import creator from "./creator.png";
-import mobile from "./mobile.png";
-import web from "./web.png";
-import github from "./github.png";
-import menu from "./menu.svg";
-import close from "./close.svg";
+import frontend from "/assets/frontend.png";
+import creator from "/assets/creator.png";
+import mobile from "/assets/mobile.png";
+import web from "/assets/web.png";
+import github from "/assets/github.png";
+import menu from "/assets/menu.svg";
+import close from "/assets/close.svg";
 
-import css from "./tech/css.png";
-import figma from "./tech/figma.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
+import css from "/assets/tech/css.png";
+import figma from "/assets/tech/figma.png";
+import git from "/assets/tech/git.png";
+import html from "/assets/tech/html.png";
+import javascript from "/assets/tech/javascript.png";
+import mongodb from "/assets/tech/mongodb.png";
+import nodejs from "/assets/tech/nodejs.png";
+import reactjs from "/assets/tech/reactjs.png";
+import redux from "/assets/tech/redux.png";
+import tailwind from "/assets/tech/tailwind.png";
+import typescript from "/assets/tech/typescript.png";
+import threejs from "/assets/tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import meta from "/assets/company/meta.png";
+import shopify from "/assets/company/shopify.png";
+import starbucks from "/assets/company/starbucks.png";
+import tesla from "/assets/company/tesla.png";
 
-import weather from "./weather.png";
-import reactapp from "./reactapp.png";
+import weather from "/assets/weather.png";
+import reactapp from "/assets/reactapp.png";
 import portphotofolio from "/assets/portphotofolio.png";
 
 export {
